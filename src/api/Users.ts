9f2ed9acca53cbabf2e286515cca3d35c23d7621
ubukaze1 <gabo2060@hotmail.com>
2026-06-06 15:29:47 -0500

    Ultimos cambios con el Rol MASTER

--- src/api/Users.ts
+++ src/api/Users.ts
@@ -12,7 +12,39 @@ export type UserProfile = {
   kitchenLevel: string;
 };
 
+export type User = {
+  id: number;
+  email: string;
+  allergy: string;
+  kitchenLevel: string;
+  rol: string;
+};
+
+export type CreateAdminDto = {
+  email: string;
+  password: string;
+  rol: string
+};
+
 export const usersApi = {
+  getAll: () =>
+    http<User[]>("/api/v1/users"),
+
+  update: (id: number, dto: UpdateProfileDto) =>
+    http<User>(`/api/v1/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(dto),
+    }),
+
+  delete: (id: number) =>
+    http<void>(`/api/v1/users/${id}`, { method: "DELETE" }),
+
+  createAdmin: (dto: CreateAdminDto) =>
+    http<User>("/api/v1/users/admin", {
+      method: "POST",
+      body: JSON.stringify(dto),
+    }),
+
   updateProfile: (dto: UpdateProfileDto) =>
     http<UserProfile>("/api/v1/users/profile", {
       method: "PUT",
